245490f6f976b4c0df005a02ecb7880aecb2048d
Chia Heung <chiaheung@axcell.my>
2025-03-17 09:05:18 +0800

    First Change

---
+++ app/src/main/java/my/edu/utar/studylah/MainActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utar.studylah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Testing
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
